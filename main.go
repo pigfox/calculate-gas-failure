7e7604f629564fa33cf5211ea7369e06397d3f5d
Peter Sjolin <pigfoxeria@gmail.com>
2024-09-10 09:45:30 -0700

    updated comment

--- main.go
+++ main.go
@@ -20,15 +20,19 @@ var localHost = false
 
 func main() {
 	wallet := ""
+	from := ""
+	to := ""
 	if localHost {
 		//get wallet address from anvil
 		wallet = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+		from = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+		to = "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC"
 	} else {
 		wallet = "0x478d21c5167CB66AdEDAFA8D72D1f3757F6e6206"
+		from = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+		to = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"
 	}
 
-	from := "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-	to := "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"
 	contract := "0xcBc57F275dB5fd2F4da20882fCa443B9cd302eCD"
 	token := "0x46101fbe580940c7dd2d2879662bc98954b5edd1"
 	amount := 17720
@@ -128,6 +132,7 @@ func gasCalculated(wallet, from, to, contract, token string, amount int) *big.In
 		return big.NewInt(0)
 	}
 	fmt.Println("gasLimit:", gasLimit)
+	fmt.Println("price", price(gasPrice, gasLimit)) //-->gasLimit 0
 
 	header, err := client.HeaderByNumber(ctx, nil)
 	if err != nil {
@@ -149,7 +154,14 @@ func gasCalculated(wallet, from, to, contract, token string, amount int) *big.In
 	return big.NewInt(int64(gasLimit))
 }
 
-// Deployed contract https://etherscan.io/address/0xcBc57F275dB5fd2F4da20882fCa443B9cd302eCD
+/*
+The string returned from function flashLoanReceiverABI() is the ABI of the contract deployed at
+https://etherscan.io/address/0xcBc57F275dB5fd2F4da20882fCa443B9cd302eCD
+*/
 func flashLoanReceiverABI() string {
 	return `[{"inputs":[{"internalType":"address","name":"_owner","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"initiator","type":"address"},{"internalType":"address","name":"baseTokenAddress","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"uint256","name":"fee","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"onFlashLoan","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"updateOwner","outputs":[],"stateMutability":"nonpayable","type":"function"}]`
+}
+
+func price(gasPrice *big.Int, gasLimit uint64) *big.Int {
+	return new(big.Int).Mul(gasPrice, new(big.Int).SetUint64(gasLimit))
 }
