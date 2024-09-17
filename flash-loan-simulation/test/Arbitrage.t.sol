5b2ffcdcfb87c2d80a9e69d07436cbcac93583c4
Peter Sjolin <pigfoxeria@gmail.com>
2024-09-17 16:52:33 -0700

    removed old code

--- flash-loan-simulation/test/Arbitrage.t.sol
+++ flash-loan-simulation/test/Arbitrage.t.sol
@@ -2,8 +2,6 @@
 pragma solidity 0.8.26;
 
 import {Test} from "forge-std/Test.sol";
-//import {console} from "forge-std/console.sol";
-//import {Test, console} from "forge-std/Test.sol"; //<-default from project - fails
 import {console} from "../lib/forge-std/src/console.sol"; //<-- proper path
 import {Arbitrage} from "../src/Arbitrage.sol";
 import {Dex1} from "../src/Dex1.sol";
