021a650dbcfa37ea1a217a0746625151adc86fce
Peter Sjolin <pigfoxeria@gmail.com>
2024-09-18 17:17:02 -0700

    added trace to execution

--- flash-loan-simulation/run.sh
+++ flash-loan-simulation/run.sh
@@ -3,4 +3,4 @@ set +x
 set +e
 echo "Running flash loan simulation"
 forge clean
-forge test --fork-url http://127.0.0.1:8545 --match-contract ArbitrageTest --match-test test_arbitrage -vvv
+forge test --fork-url http://127.0.0.1:8545 --match-contract ArbitrageTest --match-test test_arbitrage -vvvv
