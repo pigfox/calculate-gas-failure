e82b0ffc8696695cedf8f9139e4c7fbcbc896754
Peter Sjolin <pigfoxeria@gmail.com>
2024-09-26 16:12:48 -0700

    refactor var names

--- flash-loan-simulation/test/Arbitrage.t.sol
+++ flash-loan-simulation/test/Arbitrage.t.sol
@@ -30,38 +30,37 @@ contract ArbitrageTest is Test {
 
         xtoken.supply(address(dex1), 25000);
         xtoken.supply(address(dex2), 5000);
-        xtoken.supply(address(mfp), 100000);
+        xtoken.supply(address(mfp), 1000000);
         dex1.setTokenPrice(address(xtoken),125);
         dex2.setTokenPrice(address(xtoken),100);
-        uint256 dex1Balance = xtoken.balanceOf(address(dex1));
-        uint256 dex2Balance = xtoken.balanceOf(address(dex2));
-        uint256 dex1Value = dex1.valueOfTokens(address(xtoken));
-        uint256 dex2Value = dex2.valueOfTokens(address(xtoken));
-        uint256 dex1Price = dex1.getTokenPrice(address(xtoken));
-        uint256 dex2Price = dex2.getTokenPrice(address(xtoken));
+        uint256 dex1BalanceOf = xtoken.balanceOf(address(dex1));
+        uint256 dex2BalanceOf = xtoken.balanceOf(address(dex2));
+        uint256 dex1ValueOfTokens = dex1.valueOfTokens(address(xtoken));
+        uint256 dex2ValueOfTokens = dex2.valueOfTokens(address(xtoken));
+        uint256 dex1TokenPrice = dex1.getTokenPrice(address(xtoken));
+        uint256 dex2TokenPrice = dex2.getTokenPrice(address(xtoken));
 
-        console.log("dex1.getTokenPrice(address(xtoken)", dex1.getTokenPrice(address(xtoken)));
-        console.log("dex2.getTokenPrice(address(xtoken)", dex2.getTokenPrice(address(xtoken)));
+        console.log("dex1TokenPrice", dex1TokenPrice);
+        console.log("dex2TokenPrice", dex2TokenPrice);
 
-        if (dex1Price > dex2Price) {
+        if (dex1TokenPrice > dex2TokenPrice) {
             arbitrage = new Arbitrage(address(mfp),address(dex2), address(dex1), address(xtoken));
-            swapAmount = arbitrage.findMinimuBalance(dex2Balance, dex1Balance);
+            swapAmount = arbitrage.findMinimuBalance(dex2BalanceOf, dex1BalanceOf);
         } else {
             arbitrage = new Arbitrage(address(mfp),address(dex1), address(dex2), address(xtoken));
-            swapAmount = arbitrage.findMinimuBalance(dex1Balance, dex2Balance);
+            swapAmount = arbitrage.findMinimuBalance(dex1BalanceOf, dex2BalanceOf);
         }
 
         console.log("swapAmount:", swapAmount);
-        console.log("dex1Value:", dex1Value);
-        console.log("dex2Value:", dex2Value);
+
 
         console.log("--Before flashloan--");
         console.log("xtoken.balanceOf(address(mfp)):", xtoken.balanceOf(address(mfp)));
         console.log("xtoken.balanceOf(address(arbitrage)):", xtoken.balanceOf(address(arbitrage)));
-        console.log("xtoken.balanceOf(address(dex1)):", xtoken.balanceOf(address(dex1)));
-        console.log("xtoken.balanceOf(address(dex2)):", xtoken.balanceOf(address(dex2)));
-        console.log("dex1.valueOfTokens(address(xtoken)):", dex1.valueOfTokens(address(xtoken)));
-        console.log("dex2.valueOfTokens(address(xtoken)):", dex2.valueOfTokens(address(xtoken)));
+        console.log("dex1BalanceOf:", dex1BalanceOf);
+        console.log("dex2BalanceOf:", dex2BalanceOf);
+        console.log("dex1ValueOfTokens:", dex1ValueOfTokens);
+        console.log("dex2ValueOfTokens:", dex2ValueOfTokens);
 
         mfp.transferToken(address(xtoken), address(arbitrage), 1000);
 
